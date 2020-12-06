8b4bf063d983e689d0281f263406f0ccef317e90
ksala1 <ksala8043@gmail.com>
2020-12-07 07:09:20 +0900

    5

--- weather.js
+++ weather.js
@@ -1,6 +1,18 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "a2fc265fecf48640c3ee0008645fe4d8";
-
 const COORDS = "coords";
+
+function getWeather(lat, log){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj))
@@ -14,6 +26,7 @@ function handleGeoSucces(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError(){
@@ -25,11 +38,12 @@ navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
 }
 
 function loadCoords(){
-    const loadCords = localStorage.getItem(COORDS);
-if(loadCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+if(loadedCoords === null){
     askForCoords();
  } else {
-     //get weather
+     const parsedCooeds =JSON.parse(loadedCoords)
+     getWeather(parsedCooeds.latitude, parsedCooeds.longitude);
  }
 }
 
